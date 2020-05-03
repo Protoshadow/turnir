9886e4ff3441dafa920ddc333c5e9a0bd27b1357
Protoshadow <60407732+Protoshadow@users.noreply.github.com>
2020-05-03 17:27:13 +0200

    Add files via upload

---
+++ group.php
@@ -0,0 +1,14 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Credentials: true");
+include("database_connection.php");
+$id = $_GET['group_id'];
+
+$result = mysqli_query($db, "SELECT * FROM grupe WHERE id = $id");
+$json_array = array();
+while ($row = mysqli_fetch_assoc($result))
+{
+	$json_array[] = $row;
+}
+print(json_encode($json_array[0]));
+?>

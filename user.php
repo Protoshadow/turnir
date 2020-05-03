30667056612600a31c014629f55326b256837d87
Protoshadow <60407732+Protoshadow@users.noreply.github.com>
2020-05-03 17:51:08 +0200

    Add files via upload

--- user.php
+++ user.php
@@ -1,19 +1,15 @@
 <?php
-//user.php
 header("Access-Control-Allow-Origin: http://localhost:4200");
 header("Access-Control-Allow-Credentials: true");
 include("database_connection.php");
-if(isset($_COOKIE["type"]))
-{
-$result = mysqli_query($db, "SELECT username, email, rank, id, groupid, fname, lname FROM korisnici WHERE username = '".$_COOKIE["type"]."'");
+$id = $_GET['user_id'];
+
+$result = mysqli_query($db, "SELECT username, email, rank, id, groupid, fname, lname FROM korisnici WHERE id = $id");
 $json_array = array();
 while ($row = mysqli_fetch_assoc($result))
 {
 	$json_array[] = $row;
 }
 print(json_encode($json_array[0]));
-}
-else{
-	http_response_code(401);
-}
+mysqli_close($db);
 ?>

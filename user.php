916f476f591de08a424491340bcaa90a85becc1b
Protoshadow <60407732+Protoshadow@users.noreply.github.com>
2020-05-03 18:13:21 +0200

    Add files via upload

--- user.php
+++ user.php
@@ -2,8 +2,9 @@
 header("Access-Control-Allow-Origin: http://localhost:4200");
 header("Access-Control-Allow-Credentials: true");
 include("database_connection.php");
+if ( isset( $_GET['user_id']) ) 
+{
 $id = $_GET['user_id'];
-
 $result = mysqli_query($db, "SELECT username, email, rank, id, groupid, fname, lname FROM korisnici WHERE id = $id");
 $json_array = array();
 while ($row = mysqli_fetch_assoc($result))
@@ -12,4 +13,22 @@ while ($row = mysqli_fetch_assoc($result))
 }
 print(json_encode($json_array[0]));
 mysqli_close($db);
+}
+
+else{
+	if(isset($_COOKIE["type"]))
+{
+	$result = mysqli_query($db, "SELECT username, email, rank, id, groupid, fname, lname FROM korisnici WHERE username = '".$_COOKIE["type"]."'");
+	$json_array = array();
+	while ($row = mysqli_fetch_assoc($result))
+{
+		$json_array[] = $row;
+}
+	print(json_encode($json_array[0]));
+}
+	else{
+		http_response_code(401);
+		echo "niste ulogovani";
+}
+}
 ?>

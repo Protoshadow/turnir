ddf9039ac4f86d38920289251cbe297ba3550727
Protoshadow <60407732+Protoshadow@users.noreply.github.com>
2020-05-10 16:31:56 +0200

    Add files via upload

--- turnir.sql
+++ turnir.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 10, 2020 at 02:02 PM
+-- Generation Time: May 10, 2020 at 02:31 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `grupe` (
 --
 
 INSERT INTO `grupe` (`menager`, `groupname`, `member1`, `member2`, `member3`, `member4`, `id`) VALUES
-('proto', '', '', 'sdf', '', 'asdf', 8);
+('proto', 'imegrupe', '', 'sdf', '', 'asdf', 8);
 
 -- --------------------------------------------------------
 
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `korisnici` (
   `id` int(4) NOT NULL AUTO_INCREMENT,
   `groupid` int(5) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `korisnici`
@@ -72,7 +72,10 @@ CREATE TABLE IF NOT EXISTS `korisnici` (
 
 INSERT INTO `korisnici` (`username`, `password`, `email`, `fname`, `lname`, `rank`, `id`, `groupid`) VALUES
 ('proto', '$2y$10$cHpf3TzonURXDENRiRF0de1ycSfnM4NJ27sdwyUCf5L.sewDlkCBe', 'john_smith@gmail.com', 'John', 'Smith', '3', 1, 0),
-('protoshadow', '923352284767451ab158a387a283df26', 'boris.shared@gmail.com', 'Boris', 'Rodic', 'user', 10, 8);
+('protoshadow', '923352284767451ab158a387a283df26', 'boris.shared@gmail.com', 'Boris', 'Rodic', 'user', 10, 8),
+('neko', '923352284767451ab158a387a283df26', 'neko@negde.com', 'Neko', 'Nekic', 'user', 12, NULL),
+('neko2', '923352284767451ab158a387a283df26', 'neko2@negde.com', 'Neko2', 'Nekic2', 'user', 13, NULL),
+('neko3', '923352284767451ab158a387a283df26', 'neko3@negde.com', 'Neko3', 'Nekic3', 'admin', 14, NULL);
 
 -- --------------------------------------------------------
 

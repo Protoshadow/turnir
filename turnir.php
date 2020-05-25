d8310a73eb97df8d940ad8e4f3a9e74de7cdfb28
Protoshadow <60407732+Protoshadow@users.noreply.github.com>
2020-05-25 20:12:55 +0200

    Add files via upload

--- turnir.php
+++ turnir.php
@@ -21,7 +21,7 @@ else{
 	http_response_code(401);
 }
 }
-elseif ( isset( $_GET['turnir_showall']) ) 
+elseif ( isset( $_GET['turnir_showall'])) 
 {
 $id = $_GET['turnir_showall'];
 $result = mysqli_query($db, "SELECT * FROM turnir");
@@ -32,6 +32,29 @@ while ($row = mysqli_fetch_assoc($result))
 }
 print(json_encode($json_array));
 mysqli_close($db);
+}
+elseif ( isset( $_GET['current_user'])) 
+{
+	$query = "SELECT * FROM korisnici WHERE username ='".$_COOKIE["type"]."'";
+	$statement = $connect->prepare($query);
+	$statement->execute();
+	$count = $statement->rowCount();
+  if($count > 0)
+  {
+	$result = $statement->fetchAll();
+	$query = mysqli_query($db, "SELECT * FROM turnir WHERE r1g1 = '".$result[0]["groupid"]."' OR r1g2 = '".$result[0]["groupid"]."' OR r1g3 = '".$result[0]["groupid"]."' OR 
+	r1g4 = '".$result[0]["groupid"]."' OR r1g5 = '".$result[0]["groupid"]."' OR r1g6 = '".$result[0]["groupid"]."' OR r1g7 = '".$result[0]["groupid"]."' OR r1g8 = '".$result[0]["groupid"]."'");
+	$json_array = array();
+while ($row = mysqli_fetch_assoc($query))
+{
+	$json_array[] = $row;
+}
+print(json_encode($json_array));
+mysqli_close($db);
+}
+else{
+	http_response_code(401);
+}
 }
 else{
 	http_response_code(401);
